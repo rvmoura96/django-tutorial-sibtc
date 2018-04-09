b7fffbe1644183e044ac62230c4e6eee38932a63
Renan Moura <rvmoura.96@gmail.com>
2018-04-09 17:28:05 -0300

    fix gitignore

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\renan.fj\desktop\estudos\django_simple\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
