bc35f805cafc36bc61b263e6bf5c81a19e283790
Renan Moura <rvmoura.96@gmail.com>
2018-04-17 18:46:20 -0300

    Conclusion Sign Up module from part 4 form tutorial series.

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -3,10 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import signup
+
+from ..views import signup
+from ..forms import SignUpForm
 
 # Create your tests here.
 class SignUpTests(TestCase):
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username', 'email', 'password1', 'password2',]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
+        
     def setUp(self):
         url = reverse('signup')
         self.response = self.client.get(url)
@@ -25,11 +33,18 @@ class SignUpTests(TestCase):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
 
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
         data = {
             'username': 'kleber',
+            'email': 'kleber@gmail.com',
             'password1': 'boloqwerty1234',
             'password2': 'boloqwerty1234'
         }
@@ -43,3 +58,18 @@ class SuccessfulSignUpTests(TestCase):
         response = self.client.get(self.home_url)
         user = response.context.get('user')
         self.assertTrue(user.is_authenticated)
+
+class InvalidSignUpTest(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
