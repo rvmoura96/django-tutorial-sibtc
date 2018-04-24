9230fec54f5b52d6e4d4405887455e50cdff1bba
Renan Moura <rvmoura.96@gmail.com>
2018-04-23 22:14:05 -0300

    End of Login Module, Step 4

---
+++ myproject/boards/tests/test_templatetags.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.test import TestCase
+
+from ..templatetags.form_tags import field_type, input_class
+
+class ExampleForm(forms.Form):
+    name = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        fields = ('name', 'password')
+
+class FieldTypeTest(TestCase):
+    def test_field_widget_type(self):
+        form = ExampleForm()
+        self.assertEquals('TextInput', field_type(form['name']))
+        self.assertEquals('PasswordInput', field_type(form['password']))
+
+class InptuClassTest(TestCase):
+    def test_unbound_field_initial_state(self):
+        form = ExampleForm()
+        self.assertEquals('form-control ', input_class(form['name']))
+
+    def test_valid_bound_field(self):
+        form = ExampleForm({'name': 'Renan', 'password': 'bigbig64'})
+        self.assertEquals('form-control is-valid', input_class(form['name']))
+        self.assertEquals('form-control ', input_class(form['password']))
+
+    def test_invalid_bound_field(self):
+        form = ExampleForm({'name': 'Renan', 'password': 'lowdogo81'})
+        self.assertEquals('form-control is-valid', input_class(form['name']))
--- myproject/boards/tests/test_views.py
+++ myproject/boards/tests/test_views.py
@@ -2,9 +2,10 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 # Create your tests here.
 class HomeTests(TestCase):
