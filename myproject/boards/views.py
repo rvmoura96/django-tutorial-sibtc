6b18783b2de3ac47e1df85787c135756a309a5f2
Renan Moura <rvmoura.96@gmail.com>
2018-04-28 12:34:22 -0300

    conclusion part 5.

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
@@ -11,7 +12,8 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_update').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics':topics})
 
 @login_required
 def new_topic(request, pk):
@@ -35,6 +37,8 @@ def new_topic(request, pk):
 
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
 
 @login_required
